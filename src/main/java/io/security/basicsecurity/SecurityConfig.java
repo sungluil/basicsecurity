47f901a400580b3a0be8e0153eabffbf3c2bd686
sungluil <josungluil@naver.com>
2021-06-18 22:23:08 +0900

    인메모리 유저생성

--- src/main/java/io/security/basicsecurity/SecurityConfig.java
+++ src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -1,33 +1,23 @@
 package io.security.basicsecurity;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.web.authentication.AuthenticationFailureHandler;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import org.springframework.security.web.authentication.logout.LogoutHandler;
-import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import java.io.IOException;
 
 @Configuration     // 설정클래스
 @EnableWebSecurity // 웹보안 활성화
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final UserDetailsService userDetailsService;
-
-    public SecurityConfig(UserDetailsService userDetailsService) {
-        this.userDetailsService = userDetailsService;
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user")
+                .password("{noop}1111")//{noop}평문으로 그대로 암호화없이 패스워드 set
+                .roles("USER");
+        auth.inMemoryAuthentication().withUser("sys").password("{noop}1111").roles("SYS");
+        auth.inMemoryAuthentication().withUser("admin").password("{noop}1111").roles("ADMIN");
     }
 
     @Override
@@ -40,14 +30,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         // TODO 인증
         http
                 .formLogin();
-
-        http
-                // SessionCreationPolicy.ALWAYS : 시큐리티가 항상 세션 생성
-                // SessionCreationPolicy.IF_REQUIRED : 필요시 생성(기본값)
-                // SessionCreationPolicy.Never : 생성하지않지만 이미 존재하면 사용
-                // SessionCreationPolicy.Stateless : 시큐리티가 생성하지않고 존재해도 사용하지않음
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED);
 
     }
 }
